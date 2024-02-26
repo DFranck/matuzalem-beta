2aa352d9328ffc423e9c5f5244688ff72ff73c9a
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-02-26 22:54:33 +0700

    add catch signup

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,4 +1,6 @@
-import { prisma } from "@/lib/prisma";
+import { Prisma, PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 import { SignupSchema } from "@/lib/schemas";
 import bcrypt from "bcrypt";
 import { NextApiRequest, NextApiResponse } from "next";
@@ -33,7 +35,15 @@ export default async function signup(
       lastName: newUser.lastName,
     });
   } catch (error) {
+    // Améliorer la gestion des erreurs pour distinguer les erreurs Prisma
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // Vérifier si l'erreur est une violation de contrainte unique (exemple : email déjà utilisé)
+      if (error.code === "P2002") {
+        return res.status(409).json({ message: "Email already in use" });
+      }
+    }
     console.error(error);
-    return res.status(500).json({ message: "An error occurred" });
+    // Retourner une réponse plus générale au client pour éviter de divulguer des détails d'erreur sensibles
+    return res.status(500).json({ message: "Failed to create user" });
   }
 }
